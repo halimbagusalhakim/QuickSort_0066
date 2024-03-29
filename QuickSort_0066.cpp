c87b495e22782f36b90f5f27a801624110c97161
halimbagusalhakim <halsat132@gmail.com>
2024-03-29 19:45:15 +0700

    membuat looping untuk input elemen array

--- QuickSort_0066.cpp
+++ QuickSort_0066.cpp
@@ -17,10 +17,18 @@ void input() {
         else
             cout << "\nMaksimum panjang array adalah 20" << endl;
     }
-
+    cout << "\n-----------------" << endl;
+    cout << "\nEnter array element" << endl;
+    cout << "\n------------------" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
     
 
 }
+
+
 
 int main()
 {
