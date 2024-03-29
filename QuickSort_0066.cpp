9769f335d40b69f3a2299ffad46e823721a12fed
halimbagusalhakim <halsat132@gmail.com>
2024-03-29 19:41:44 +0700

    membuat prosedur input

--- QuickSort_0066.cpp
+++ QuickSort_0066.cpp
@@ -7,6 +7,21 @@ int cmp_count = 0;
 int mov_count = 0;
 int n;
 
+void input() {
+    while (true) {
+        cout << " Masukan Panjang Element array :";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
+    }
+
+    
+
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
